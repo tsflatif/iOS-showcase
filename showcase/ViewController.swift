6118f0b1c51eb3f361bcef8b3ecba200e318dbf9
tsflatif <tsflatif@gmail.com>
2016-03-19 23:02:47 -0400

    Initial Commit

---
+++ showcase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  showcase
+//
+//  Created by Tauseef Latif on 2016-03-19.
+//  Copyright © 2016 Tauseef Latif. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
