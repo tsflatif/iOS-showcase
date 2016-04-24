fb298f7711d4cbd947ea3980cae093ce6de90e66
tsflatif <tsflatif@gmail.com>
2016-04-24 19:10:40 -0400

    upload picture to imageshack

--- showcase/FeedVC.swift
+++ showcase/FeedVC.swift
@@ -8,27 +8,38 @@
 
 import UIKit
 import Firebase
+import Alamofire
 
-class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var postField: MaterialTextField!
+    @IBOutlet weak var imageSelectorImg: UIImageView!
     var posts = [Post]()
+    
+    var imagePicker: UIImagePickerController!
+    
+    static var imageCache = NSCache()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.estimatedRowHeight = 358
+        
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
         
         DataService.ds.REF_POSTS.observeEventType(.Value, withBlock: { snapshot in
-            print(snapshot.value)
+            //print(snapshot.value)
             
             self.posts = []
             
             if let snapshots = snapshot.children.allObjects as? [FDataSnapshot] {
                 
                 for snap in snapshots {
-                    print("SNAP: \(snap)")
+                    //print("SNAP: \(snap)")
                     
                     if let postDict = snap.value as? Dictionary<String, AnyObject> {
                         let key = snap.key
@@ -53,14 +64,76 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         let post = posts[indexPath.row]
-        print(post.postDescription)
         
         if let cell = tableView.dequeueReusableCellWithIdentifier("PostCell") as? PostCell {
-            cell.configurCell(post)
+            cell.request?.cancel()
+            
+            var img: UIImage?
+            
+            if let url = post.imageUrl {
+                img = FeedVC.imageCache.objectForKey(url) as? UIImage
+            }
+            
+            cell.configurCell(post,img: img)
             return cell
         } else {
             return PostCell()
         }
     }
-
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        let post = posts[indexPath.row]
+        
+        if post.imageUrl == nil {
+            return 150
+        } else {
+            return tableView.estimatedRowHeight
+        }
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        imagePicker.dismissViewControllerAnimated(true, completion: nil)
+        imageSelectorImg.image = image
+    }
+    @IBAction func selectImage(sender: AnyObject) {
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    @IBAction func makePost(sender: AnyObject) {
+        if let txt = postField.text where txt != "" {
+            
+            if let img = imageSelectorImg.image {
+                let urlStr = "https://post.imageshack.us/upload_api.php"
+                let url = NSURL(string: urlStr)!
+                let imgData = UIImageJPEGRepresentation(img, 0.2)!
+                let keyData = "12DJKPSU5fc3afbd01b1630cc718cae3043220f3".dataUsingEncoding(NSUTF8StringEncoding)!
+                let keyJSON = "json".dataUsingEncoding(NSUTF8StringEncoding)!
+                
+                Alamofire.upload(.POST, url, multipartFormData: { multipartFormData in
+                    
+                    multipartFormData.appendBodyPart(data: imgData, name: "fileupload", fileName: "image", mimeType: "image/jpg")
+                    multipartFormData.appendBodyPart(data: keyData, name: "key")
+                    multipartFormData.appendBodyPart(data: keyJSON, name: "format")
+                    
+                }) { encodingResult in
+                    
+                    switch encodingResult {
+                    case .Success(let upload, _, _):
+                        
+                        upload.responseJSON(completionHandler: { response in
+                            if let info = response.result.value as? Dictionary<String, AnyObject> {
+                                
+                                if let links = info["links"] as? Dictionary<String, AnyObject> {
+                                    if let imgLink = links["image_link"] as? String {
+                                        print("LINK: \(imgLink)")
+                                    }
+                                }
+                            }
+                        })
+                    case .Failure(let error):
+                        print(error)
+                    }
+                }
+            }
+        }
+    }
 }
--- showcase/PostCell.swift
+++ showcase/PostCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class PostCell: UITableViewCell {
     
@@ -16,6 +17,7 @@ class PostCell: UITableViewCell {
     @IBOutlet weak var likesLbl: UILabel!
     
     var post: Post!
+    var request: Request?
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -29,10 +31,27 @@ class PostCell: UITableViewCell {
         showcaseImg.clipsToBounds = true
     }
     
-    func configurCell(post: Post) {
+    func configurCell(post: Post, img: UIImage?) {
         self.post = post
         self.descriptionText.text = post.postDescription
         self.likesLbl.text = "\(post.likes)"
+        
+        if post.imageUrl != nil {
+            if img != nil {
+                self.showcaseImg.image = img
+            } else {
+                request = Alamofire.request(.GET, post.imageUrl!).validate(contentType: ["image/*"]).response(completionHandler: { request, response, data, err in
+                    
+                    if err == nil {
+                        let img = UIImage(data: data!)!
+                        self.showcaseImg.image = img
+                        FeedVC.imageCache.setObject(img, forKey: self.post.imageUrl!)
+                    }
+                })
+            }
+        } else {
+            self.showcaseImg.hidden = true
+        }
         
     }
 
